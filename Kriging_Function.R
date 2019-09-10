e85c958896c781465c0dacbd3bceba3e17e08b38
Tim Loftus <Tim.Loftus@accuweather.com>
2019-09-10 12:26:20 -0400

    Add files via upload
    Function for Kriging in R - some of the below scripts are just used as testing inputs.

--- Kriging_Function.R
+++ Kriging_Function.R
@@ -15,6 +15,61 @@ install.packages("maptools")
 install.packages("sf")
 install.packages("automap")
 
+
+#Begin Modular Function*******************************************************************************
+#Inputs Would be Forecasted Column Name: frc_var, Forecasted Data Layer: merged_frc, Grid Cell Size: cell_size
+# Extent: Polygon Boundary
+#Declare Kriging Function:
+station_kriging <- function(frcvar, merged_frc, cell_size, grd_extent) {
+  library(dplyr)
+  library(gstat)
+  library(sp)
+  library(rgdal)
+  library(automap)
+  library(ggplot2)
+  library(gridExtra)
+  library(OpenStreetMap)
+  library(rJava)
+  library(OSMscale)
+  library(raster)
+  #Create Gridded Forecast Layer:
+  min_lon <- min(merged_frc$coords.x1)
+  max_lon <- max(merged_frc$coords.x1)
+  min_lat <- min(merged_frc$coords.x2)
+  max_lat <- max(merged_frc$coords.x2)
+  
+  Longitude.range <- as.numeric(c(min_lon,max_lon))
+  Latitude.range <- as.numeric(c(min_lat,max_lat))
+  
+  grd <- expand.grid(Longitude = seq(from = Longitude.range[1], to = Longitude.range[2], by = 0.1), 
+                     Latitude = seq(from = Latitude.range[1],
+                                    to = Latitude.range[2], by = 0.1))  # expand points to grid
+  
+  #Convert Grid to a Spatial Pixels Data Frame:
+  grd_frame = SpatialPixelsDataFrame(points = grd[c("Longitude", "Latitude")], data = grd)
+  
+  #Projected Coordinates:
+  proj4string(merged_frc) <- proj4string(grd_frame)
+  proj4string(grd_extent) <- proj4string(grd_frame)#make sure the shapefile has the same CRS from the data, and from the prediction grid.
+  
+  #Clip the prediction grid with the shapefile.
+  grd_subset <- grd_frame[grd_extent,]
+  proj4string(grd_subset) <- CRS(proj4string(grd_extent))
+  
+  #IF Statement When Values are All 0 for Vectors:
+  if(all(frcvar==0)){
+  grd_subset["var1.pred"] <- NA
+  grd_subset$var1.pred <- 0 
+  kriging_result  <- as(grd_subset, "SpatialPixels")
+  } else {
+  kriging_result = autoKrige(frcvar~1, merged_frc, grd_subset)
+  }
+  plot(kriging_result)
+
+}
+#Function Ends**************************************************************************************
+
+#Function Inputs for Kriging:
 #Upstream processes to prep for kriging function, this is used for testing purposes
 #Set Inputs:
 library(rgdal)
@@ -36,54 +91,17 @@ conn <- dbConnect(odbc(),
 library(data.table)
 data <- dbGetQuery(conn, "SELECT vw_DayNight.STATION_CODE, vw_DayNight.DATE, vw_DayNight.DAY_FLAG,
                    vw_DayNight.RAIN, vw_DayNight.SNOW, vw_DayNight.ICE,
-                   vw_DayNight.TSTORM_POP FROM vW_DayNight WHERE vw_DayNight.DAY_FLAG='D' AND vw_DayNight.DATE = '2019-09-02'")
+                   vw_DayNight.TSTORM_POP FROM vW_DayNight WHERE vw_DayNight.DAY_FLAG='D' AND vw_DayNight.DATE = '2019-09-12'")
 frcdata <- setDT(data, keep.rownames = TRUE, check.names = FALSE)
 
 #Merge forecast data to station file; station by station name; stat_col:
 merged_frc <- merge(station, frcdata, by.x="STATION_CO", by.y="STATION_CODE")
 
 View(merged_frc)
-barplot(table(merged_frc$TSTORM_POP))
+barplot(table(merged_frc$RAIN))
 class(merged_frc)
 
-
-#Begin Modular Function:
-#Inputs Would be Forecasted Column Name; frc_var, forecasted data layer; merged_frc; grid cell size; cell_size
-station_kriging <- function(frcvar, merged_frc, cell_size) {
-  library(dplyr)
-  library(gstat)
-  library(sp)
-  library(rgdal)
-  library(automap)
-  library(ggplot2)
-  library(gridExtra)
-  library(OpenStreetMap)
-  library(rJava)
-  library(OSMscale)
-  #Create Gridded Forecast Layer:
-  min_lon <- min(merged_frc$coords.x1)
-  max_lon <- max(merged_frc$coords.x1)
-  min_lat <- min(merged_frc$coords.x2)
-  max_lat <- max(merged_frc$coords.x2)
-  
-  Longitude.range <- as.numeric(c(min_lon,max_lon))
-  Latitude.range <- as.numeric(c(min_lat,max_lat))
-  
-  grd <- expand.grid(Longitude = seq(from = Longitude.range[1], to = Longitude.range[2], by = 0.1), 
-                     Latitude = seq(from = Latitude.range[1],to = Latitude.range[2], by = 0.1))  # expand points to grid
-  
-  #Get long and lat from data.frame.  
-  xy <- grd[,c(1,2)]
-  spdf <- SpatialPointsDataFrame(coords = xy, data = grd, 
-                                 proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-  #Kriging Process Continued -  
-  spdf.proj <- spTransform(spdf, CRS("+proj=utm +zone=14 +datum=WGS84"))
-  frc.proj <- spTransform(merged_frc, CRS("+proj=utm +zone=14 +datum=WGS84"))
-  kriging_result =autoKrige(frcvar~1, frc.proj, spdf.proj)
-  plot(kriging_result)
-}
-
-#Function Inputs for Kriging:
-frcvar <- merged_frc$TSTORM_POP
-forecast <- station_kriging(frcvar, merged_frc, 0.1)
+frcvar <- merged_frc$RAIN
+grd_extent <- readOGR(myDir, "KS_Extent")
+forecast <- station_kriging(frcvar, merged_frc, 0.1, grd_extent)
 forecast
